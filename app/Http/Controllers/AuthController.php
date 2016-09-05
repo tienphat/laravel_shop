90de1c0c36aceda2d6dc300b4f64b9e90b29521f
tienphat <quangphatc3@gmail.com>
2016-09-06 06:54:11 +0700

    '...'

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,35 +17,23 @@ class AuthController extends Controller
 
     public function handleLogin(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|exists:users,name,role,admin',
-            'password' => 'required'
-        ]);
 
-        $username = $req['username'];
-        $password = $req['password'];
+        // $this->validate($request, [
+        //     'name' => 'required|exists:users,name,role,admin',
+        //     'password' => 'required'
+        // ]);
+
+        $username = $request['username'];
+        $password = $request['password'];
         if (Auth::attempt(['name' => $username, 'password' => $password]))
         {
             return redirect('/admin');
         }
         else
         {
-            // Your logic of invalid credentials.
             echo ('<script>alert("Thông tin đăng nhập không chính xác, vui lòng nhập lại!")</script>');
             return view('login');
         }
-//        
-//        $username = $req['username'];
-//        $password = $req['password'];
-//        if (Auth::attempt(['name' => $username, 'password' => $password]))
-//        {
-//            return view('backend.index');
-//        }
-//        else
-//        {
-//            echo ('<script>alert("Thông tin đăng nhập không chính xác, vui lòng nhập lại!")</script>');
-//            return view('login');
-//        }
     }
 
 }
